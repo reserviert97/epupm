ef2cdba856832ac024d54698933152e361c17206
ardhi <nurlatif97@outlook.com>
2019-04-03 17:45:23 +0700

    first init

---
+++ database/migrations/2019_04_03_104025_create_table_pembelian.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTablePembelian extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('pembelian', function (Blueprint $table) {
+            //
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('pembelian', function (Blueprint $table) {
+            //
+        });
+    }
+}
